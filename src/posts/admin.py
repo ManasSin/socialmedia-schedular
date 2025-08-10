23716f9f55c8628d4ace30a9896105ade73774e0
ManasSin <maltisadan12@gmail.com>
2025-08-10 19:39:02 +0530

    Implement custom PostAdmin for enhanced admin functionality
    - Added a custom `PostAdmin` class to manage the Post model in the Django admin interface.
    - Introduced filtering and display options based on user permissions.
    - Customized queryset and readonly fields based on user roles.
    - Implemented delete permission logic to restrict deletion based on user ownership and sharing status.
    - Enhanced model saving to automatically assign the user on creation.

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,7 +1,64 @@
+from typing import Any
 from django.contrib import admin
+from django.http import HttpRequest
 
 # Register your models here.
 
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ["updated_at"]
+    list_display = ["user", "content", "updated_at"]
+    # pass
+
+    def get_list_display(self, request: HttpRequest):
+        if request.user.is_superuser:
+            return ["content", "user", "updated_at"]
+        # return super().get_list_display(request)
+        return ["content", "updated_at"]
+
+    def get_queryset(self, request: HttpRequest):
+        # if request.user.is_superuser:
+        #     return super().get_queryset(request)
+        # return super().get_queryset(request).filter(user=request.user)
+        user = request.user
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=user)
+
+    def get_readonly_fields(
+        self, request: HttpRequest, obj: Any | None = ...
+    ) -> list[str]:
+        print(obj)
+        if obj and obj.shared_at_linkedin:
+            return ["user", "content", "shared_at_linkedin", "share_on_linkedin"]
+        if request.user.is_superuser:
+            return ["shared_at_linkedin"]
+        return ["user", "shared_at_linkedin"]
+
+    def has_delete_permission(
+        self,
+        request: HttpRequest,
+        obj: Any | None = None,
+    ) -> bool:
+        if request.user.is_superuser:
+            return True
+        if obj is None:
+            return False
+        return obj.user == request.user and not obj.shared_at_linkedin
+        # return False
+
+    def save_model(
+        self, request: HttpRequest, obj: Any, form: Any, change: Any
+    ) -> None:
+        print(form)
+        if not change:
+            form.user = request.user
+            if not obj.user:
+                obj.user = request.user
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Post, PostAdmin)
